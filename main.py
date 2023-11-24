1192e6a0cc526e0b0b518e4a1137bcb56578152b
hustincao <40309113+hustincao@users.noreply.github.com>
2023-11-23 23:51:56 -0500

    bug fix

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import sys
 from numba import cuda
 import numpy as np
 from itertools import combinations
+import time
 
-UNFILLED = 0 # White
-FILLED = 1 # Black
+UNFILLED = 0  # White
+FILLED = 1  # Black
 UNKNOWN = 6
+
 
 def generate_row_possibilities(HEIGHT, WIDTH, ROW_CONSTRAINTS, row_answers):
     # Generate all possible row answers
@@ -14,12 +16,15 @@ def generate_row_possibilities(HEIGHT, WIDTH, ROW_CONSTRAINTS, row_answers):
         n_empty = WIDTH - sum(ROW_CONSTRAINTS[r]) - n_groups + 1
         possible_row_answers = np.ndarray(shape=(0, WIDTH), dtype=np.int32)
         for p in combinations(range(n_groups + n_empty), n_groups):
-            possible_answer = np.ones(shape=(WIDTH), dtype=np.int32)
+            possible_answer = np.full(
+                shape=(WIDTH), fill_value=UNFILLED, dtype=np.int32)
+            total_sum = 0
             for i in range(len(p)):
-                start_index = p[i]
+                start_index = total_sum + p[i]
                 length = ROW_CONSTRAINTS[r][i]
                 for index in range(length):
-                    possible_answer[start_index + index + i] = 2
+                    possible_answer[start_index + index] = FILLED
+                total_sum += length
             possible_row_answers = np.vstack(
                 (possible_row_answers, possible_answer))
         row_answers[r] = possible_row_answers
@@ -31,12 +36,16 @@ def generate_col_possibilities(HEIGHT, WIDTH, COL_CONSTRAINTS, col_answers):
         n_empty = HEIGHT - sum(COL_CONSTRAINTS[c]) - n_groups + 1
         possible_col_answers = np.ndarray(shape=(0, HEIGHT), dtype=np.int32)
         for p in combinations(range(n_groups + n_empty), n_groups):
-            possible_answer = np.ones(shape=(HEIGHT), dtype=np.int32)
+            possible_answer = np.full(
+                shape=(HEIGHT), fill_value=UNFILLED, dtype=np.int32)
+            total_sum = 0
             for i in range(len(p)):
-                start_index = p[i]
+                start_index = total_sum + p[i]
                 length = COL_CONSTRAINTS[c][i]
                 for index in range(length):
-                    possible_answer[start_index + index + i] = 2
+                    possible_answer[start_index + index] = FILLED
+                total_sum += length
+
             possible_col_answers = np.vstack(
                 (possible_col_answers, possible_answer))
         col_answers[c] = possible_col_answers
@@ -44,52 +53,49 @@ def generate_col_possibilities(HEIGHT, WIDTH, COL_CONSTRAINTS, col_answers):
 
 def intersect_row_possibilities(HEIGHT, WIDTH, row_answers, board):
     for r in range(HEIGHT):
-        line = np.zeros(shape=(WIDTH), dtype=np.int32)
+        line = np.full(shape=(WIDTH), fill_value=UNKNOWN, dtype=np.int32)
 
         for i in range(WIDTH):
             line[i] = row_answers[r][0][i]
         for j in range(1, len(row_answers[r])):
             for i in range(WIDTH):
                 if line[i] != row_answers[r][j][i]:
-                    line[i] = 0
+                    line[i] = UNKNOWN
 
         for i in range(WIDTH):
-            if board[r][i] == 0:
+            if board[r][i] == UNKNOWN:
                 board[r][i] = line[i]
-            elif line[i] != 0 and board[r][i] != line[i]:
+            elif line[i] != UNKNOWN and board[r][i] != line[i]:
                 print("ERROR: Illegal constraints")
 
 
 def intersect_col_possibilities(HEIGHT, WIDTH, col_answers, board):
+
     for c in range(WIDTH):
-        line = np.zeros(shape=(HEIGHT), dtype=np.int32)
+        line = np.full(shape=(HEIGHT), fill_value=UNKNOWN, dtype=np.int32)
         for i in range(HEIGHT):
             line[i] = col_answers[c][0][i]
         for j in range(1, len(col_answers[c])):
             for i in range(HEIGHT):
                 if line[i] != col_answers[c][j][i]:
-                    line[i] = 0
+                    line[i] = UNKNOWN
 
         for i in range(WIDTH):
-            if board[i][c] == 0:
+            if board[i][c] == UNKNOWN:
                 board[i][c] = line[i]
-            elif line[i] != 0 and board[i][c] != line[i]:
+            elif line[i] != UNKNOWN and board[i][c] != line[i]:
                 print("ERROR: Illegal constraints")
 
 
 def remove_row_possibilities(HEIGHT, WIDTH, row_answers, board):
-    # With new board, delete impossible answers
     for r in range(HEIGHT):
         for row in reversed(range(len(row_answers[r]))):
-            # print(row)
             answer = row_answers[r][row]
-            # print(answer)
             for i in range(WIDTH):
-                if board[r][i] != 0 and board[r][i] != answer[i]:
+                if board[r][i] != UNKNOWN and board[r][i] != answer[i]:
                     # print(f"delete answer {row} in row {r}")
                     row_answers[r] = np.delete(row_answers[r], row, 0)
                     break
-            # print(row)
 
 
 def remove_col_possibilities(HEIGHT, WIDTH, col_answers, board):
@@ -97,7 +103,7 @@ def remove_col_possibilities(HEIGHT, WIDTH, col_answers, board):
         for col in reversed(range(len(col_answers[c]))):
             answer = col_answers[c][col]
             for i in range(HEIGHT):
-                if board[i][c] != 0 and board[i][c] != answer[i]:
+                if board[i][c] != UNKNOWN and board[i][c] != answer[i]:
                     # print(f"delete answer {col} in col {c}")
                     col_answers[c] = np.delete(col_answers[c], col, 0)
                     break
@@ -106,7 +112,7 @@ def remove_col_possibilities(HEIGHT, WIDTH, col_answers, board):
 def is_completed(HEIGHT, WIDTH, board):
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            if board[i][j] == 0:
+            if board[i][j] == UNKNOWN:
                 return False
     return True
 
@@ -120,45 +126,50 @@ def is_correct(HEIGHT, WIDTH, board, solution):
 
 
 def main():
-    file1 = open('tests/0/constraints', 'r')
-    lines = file1.readlines()
-    HEIGHT, WIDTH = [int(s) for s in lines[0].strip().split()]
-    print(HEIGHT, WIDTH)
+    start_time = time.time()
     ROW_CONSTRAINTS = []
     COL_CONSTRAINTS = []
 
+    filename = 'tests/11'
+
+    constraint_file = open(f'{filename}/constraints', 'r')
+    lines = constraint_file.readlines()
+    HEIGHT, WIDTH = [int(s) for s in lines[0].strip().split()]
     for index, line in enumerate(lines[1:]):
         if index < HEIGHT:
-            COL_CONSTRAINTS.append([int(s) for s in line.strip().split( )])
+            COL_CONSTRAINTS.append([int(s) for s in line.strip().split()])
         else:
-            ROW_CONSTRAINTS.append([int(s) for s in line.strip().split( )])
-    # print(ROW_CONSTRAINTS)
-    # print(COL_CONSTRAINTS)
-    # sys.exit()
-    # ROW_CONSTRAINTS = [[2], [1, 2], [3], [2], [1, 1, 1]]
-    # COL_CONSTRAINTS = [[1, 1], [2], [1, 1], [3], [4]]
-    # solution = np.array([[2, 2, 1, 1, 1], 
-    #                      [1, 2, 1, 2, 2], 
-    #                      [1, 1, 2, 2, 2], 
-    #                      [1, 1, 1, 2, 2], 
-    #                      [2, 1, 2, 1, 2]])
+            ROW_CONSTRAINTS.append([int(s) for s in line.strip().split()])
 
-    # WIDTH = len(COL_CONSTRAINTS)
-    # HEIGHT = len(ROW_CONSTRAINTS)
+    if len(COL_CONSTRAINTS) != WIDTH or len(ROW_CONSTRAINTS) != HEIGHT:
+        print("number of constraints not equal to board size")
+        sys.exit()
 
-    board = np.zeros(shape=(HEIGHT, WIDTH), dtype=np.int32)
-    # print(board)
-    # row_answers = np.ndarray(shape=(5), dtype=np.dtype(object))
+    solution = np.ndarray(shape=(HEIGHT, WIDTH), dtype=np.int32)
 
-    row_answers = np.full(shape=(HEIGHT), fill_value=[0 for _ in range(HEIGHT)], dtype=np.dtype(object))
+    solution_file = open(f'{filename}/solution', 'r')
+    lines = solution_file.readlines()
+    for i, line in enumerate(lines):
+        for j, value in enumerate(line.strip()):
+            solution[i][j] = value
+
+    constraint_file.close()
+    solution_file.close()
+
+    board = np.full(shape=(HEIGHT, WIDTH), dtype=np.int32,
+                    fill_value=[UNKNOWN for _ in range(HEIGHT)])
+
+    row_answers = np.full(shape=(HEIGHT), fill_value=[
+                          _ for _ in range(HEIGHT)], dtype=np.dtype(object))
     col_answers = np.full(shape=(WIDTH), fill_value=[
-                          0 for _ in range(WIDTH)], dtype=np.dtype(object))
+                          _ for _ in range(WIDTH)], dtype=np.dtype(object))
 
     generate_row_possibilities(HEIGHT, WIDTH, ROW_CONSTRAINTS, row_answers)
     generate_col_possibilities(HEIGHT, WIDTH, COL_CONSTRAINTS, col_answers)
 
     counter = 0
     while True:
+
 
         intersect_row_possibilities(HEIGHT, WIDTH, row_answers, board)
         intersect_col_possibilities(HEIGHT, WIDTH, col_answers, board)
@@ -169,114 +180,12 @@ def main():
         if is_completed(HEIGHT, WIDTH, board) or counter > 10:
             break
 
-    print(board)
-
-    # Generate all possible row answers
-    # for r in range(HEIGHT):
-    #     n_groups = len(ROW_CONSTRAINTS[r])
-    #     n_empty = WIDTH - sum(ROW_CONSTRAINTS[r]) - n_groups + 1
-    #     possible_row_answers = np.ndarray(shape=(0, WIDTH), dtype=np.int32)
-    #     for p in combinations(range(n_groups + n_empty), n_groups):
-    #         possible_answer = np.ones(shape=(WIDTH), dtype=np.int32)
-    #         for i in range(len(p)):
-    #             start_index = p[i]
-    #             length = ROW_CONSTRAINTS[r][i]
-    #             for index in range(length):
-    #                 possible_answer[start_index + index + i] = 2
-    #         possible_row_answers = np.vstack((possible_row_answers, possible_answer))
-    #     row_answers[r] = possible_row_answers
-
-    # # Generate all possible col answers
-    # for c in range(WIDTH):
-    #     n_groups = len(COL_CONSTRAINTS[c])
-    #     n_empty = HEIGHT - sum(COL_CONSTRAINTS[c]) - n_groups + 1
-    #     possible_col_answers = np.ndarray(shape=(0, HEIGHT), dtype=np.int32)
-    #     for p in combinations(range(n_groups + n_empty), n_groups):
-    #         possible_answer = np.ones(shape=(HEIGHT), dtype=np.int32)
-    #         for i in range(len(p)):
-    #             start_index = p[i]
-    #             length = COL_CONSTRAINTS[c][i]
-    #             for index in range(length):
-    #                 possible_answer[start_index + index + i] = 2
-    #         possible_col_answers = np.vstack((possible_col_answers, possible_answer))
-    #     col_answers[c] = possible_col_answers
-
-    # # Intersect Row
-    # for r in range(HEIGHT):
-    #     line = np.zeros(shape=(WIDTH), dtype=np.int32)
-
-    #     for i in range(WIDTH):
-    #         line[i] = row_answers[r][0][i]
-    #     for j in range(1, len(row_answers[r])):
-    #         for i in range(WIDTH):
-    #             if line[i] != row_answers[r][j][i]:
-    #                 line[i] = 0
-
-    #     for i in range(WIDTH):
-    #         if board[r][i] == 0:
-    #             board[r][i] = line[i]
-    #         elif line[i] != 0 and board[r][i] != line[i]:
-    #             print("ERROR: Illegal constraints")
-
-    # # Intersect Column
-    # for c in range(WIDTH):
-    #     line = np.zeros(shape=(HEIGHT), dtype=np.int32)
-    #     for i in range(HEIGHT):
-    #         line[i] = col_answers[c][0][i]
-    #     for j in range(1, len(col_answers[c])):
-    #         for i in range(HEIGHT):
-    #             if line[i] != col_answers[c][j][i]:
-    #                 line[i] = 0
-
-    #     for i in range(WIDTH):
-
-    #         if board[i][c] == 0:
-    #             board[i][c] = line[i]
-    #         elif line[i] != 0 and board[i][c] != line[i]:
-    #             print("ERROR: Illegal constraints")
-    # print(board)
-    # # print("---------------------------------------")
-    # # for r in row_answers:
-    # #     print(r)
-    # # print("---------------------------------------")
-
-    # print("---------------------------------------")
-    # for c in col_answers:
-    #     print(c)
-    # print("---------------------------------------")
-
-    # # With new board, delete impossible answers
-    # for r in range(HEIGHT):
-    #     for row in reversed(range(len(row_answers[r]))):
-    #         # print(row)
-    #         answer = row_answers[r][row]
-    #         # print(answer)
-    #         for i in range(WIDTH):
-    #             if board[r][i] != 0 and board[r][i] != answer[i]:
-    #                 print(f"delete answer {row} in row {r}")
-    #                 row_answers[r] = np.delete(row_answers[r], row, 0)
-    #                 break
-    #         # print(row)
-    # for c in range(WIDTH):
-    #     for col in reversed(range(len(col_answers[c]))):
-    #         answer = col_answers[c][col]
-    #         for i in range(HEIGHT):
-    #             if board[i][c] != 0 and board[i][c] != answer[i]:
-    #                 print(f"delete answer {col} in col {c}")
-    #                 col_answers[c] = np.delete(col_answers[c], col, 0)
-    #                 break
-
-    # # print("---------------------------------------")
-    # # for r in row_answers:
-    # #     print(r)
-    # # print("---------------------------------------")
-
-    # print("---------------------------------------")
-    # for c in col_answers:
-    #     print(c)
-    # print("---------------------------------------")
+    print(is_correct(HEIGHT, WIDTH, board, solution))
+    
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     # print(board)
+    # print(solution)
 
 
 if __name__ == '__main__':
